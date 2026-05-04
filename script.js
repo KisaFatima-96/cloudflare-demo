fe1c7eaba4e2e1bb40cbc3a3f479f233815fe615
KisaFatima-96 <151384965+KisaFatima-96@users.noreply.github.com>
2026-05-04 07:57:19 -0700

    Refactor greeting function to new message function

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-async function getGreeting() {
+/*async function getGreeting() {
     const name = document.getElementById("nameInput").value;
 
     const response = await fetch(`https://YOUR_WORKER_URL?name=${name}`);
     const data = await response.text();
 
     document.getElementById("result").innerText = data;
+}*/
+async function getMessage() {
+  const res = await fetch("https://black-moon-9cbf.kisaf572.workers.dev");
+  const data = await res.json();
+
+  document.getElementById("msg").innerText = data.message;
 }
